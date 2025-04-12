a5829b88cd7e843d0e5a289035899856b66f0dd6
Bobby <bobbyupreti1234@gmail.com>
2025-04-13 00:30:14 +0530

    New team member added

--- frontendrunr/src/components/Team/TeamMember.jsx
+++ frontendrunr/src/components/Team/TeamMember.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./TeamSection.css";
 
-const TeamMember = ({ image, name, role }) => {
+const TeamMember = ({ image, name, role, position }) => {
   return (
     <div className="team-member">
-      <img src={image} alt={name} className="team-image" />
+      <img
+        src={image}
+        alt={name}
+        className="team-image"
+        style={{ objectPosition: position }}
+      />
       <h3 className="team-name">{name}</h3>
       <p className="team-role">{role}</p>
     </div>
--- frontendrunr/src/components/Team/TeamSection.jsx
+++ frontendrunr/src/components/Team/TeamSection.jsx
@@ -2,8 +2,45 @@ import React, { useEffect } from "react";
 import TeamMember from "./TeamMember";
 import "./TeamSection.css";
 import AOS from "aos";
-import profileImage from "./team-member.jpeg";
 import Heading from "../../Common/Headings/Heading";
+
+const teamMembers = [
+  {
+    image:
+      "https://res.cloudinary.com/dlf8u5l7a/image/upload/v1744476820/runr/i9bdbxix2zb7ig3cx36e.jpg",
+    name: "MANOJ SHAH",
+    role: "(DIRECTOR)",
+    position: "center 30%",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dlf8u5l7a/image/upload/v1744476821/runr/z8eoesxbiylwcq5ygmfr.jpg",
+    name: "DEVANSH SHAH",
+    role: "(DIRECTOR)",
+    position: "center 15%",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dlf8u5l7a/image/upload/v1744476819/runr/m4603wohwkuz0vova6fo.jpg",
+    name: "SETUL SHAH",
+    role: "(DIRECTOR)",
+    position: "center 25%",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dlf8u5l7a/image/upload/v1744476823/runr/wml7myuwu7nwfetitx2f.jpg",
+    name: "KN SHAH",
+    role: "(DIRECTOR)",
+    position: "center 25%",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dlf8u5l7a/image/upload/v1744476814/runr/drciity5zuntkfbobsur.jpg",
+    name: "VIPUL KANABAR",
+    role: "(PARTNER)",
+    position: "center 70%",
+  },
+];
 
 const TeamSection = () => {
   useEffect(() => {
@@ -15,11 +52,15 @@ const TeamSection = () => {
     <div className="team-section">
       <Heading className="taleido_heading" id="glheading" title="Team" />
       <div className="team-members" data-aos="zoom-in-up">
-        <TeamMember
-          image={profileImage}
-          name="VIPUL KANABAR"
-          role="(PARTNER)"
-        />
+        {teamMembers?.map((member, index) => (
+          <TeamMember
+            key={index}
+            image={member.image}
+            name={member.name}
+            role={member.role}
+            position={member.position}
+          />
+        ))}
       </div>
     </div>
   );
